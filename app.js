0e9d768acb3446f0a0b336d978a46512de7f4ccb
qiushaoxi <qiushaoxi@uchain.one>
2018-04-26 15:04:55 +0800

    1st version

---
+++ app.js
@@ -0,0 +1,96 @@
+const superagent = require('superagent');
+const config = require('./config.json');
+const moment = require('moment');
+const shell = require("shelljs");
+const mail = require('./tools/mail');
+
+const restart_script = config.restart_script;
+var producer = config.producer;
+var my_last_block_timestmp = Date.now();
+
+function is_time_out(time, timeout) {
+    let gap = Date.now() - time;
+    if (gap > timeout) {
+        return true;
+    } else {
+        return false;
+    }
+
+}
+
+function safelyParseJSON(json) {
+    // This function cannot be optimised, it's best to
+    // keep it small!
+    let parsed
+
+    try {
+        parsed = JSON.parse(json)
+    } catch (e) {
+        // Oh well, but whatever...
+        logger.error(e);
+    }
+
+    return parsed // Could be undefined!
+}
+
+function restart(node) {
+    console.error("restarting", moment.unix(Date.now()));
+    mail.sendMail(node.name, node.url);
+    shell.exec(node.restart_script);
+}
+
+function check_node(node) {
+
+    let url = node.url + "/v1/chain/get_info";
+    let name = node.name;
+
+    superagent.post(url)
+        .end((err, res) => {
+            if (err) {
+                console.error(err);
+                restart(node);
+                return;
+            }
+            if (!res) {
+                console.error("res is null");
+                restart(node);
+                return;
+            }
+            let result = safelyParseJSON(res.text);
+
+            //console.log('=======================');
+            //console.log(result.head_block_producer);
+            //console.log(result.head_block_num);
+
+            let block_timestmp = moment(result.head_block_time + "Z").valueOf();
+            //console.log("time interval from last block:", ((Date.now() - block_timestmp) / 1000).toFixed(2) + " s");
+
+            let timeout_flag = is_time_out(block_timestmp, config.timeout);
+            //console.log("time out?", timeout_flag);
+            if (timeout_flag) {
+                restart(node);
+                return;
+            }
+
+            // record my block
+            if (result.head_block_producer == producer) {
+                my_last_block_timestmp = block_timestmp
+            }
+            //console.log("my_last_block_time:", ((Date.now() - my_last_block_timestmp) / 60000).toFixed(2) + " min")
+
+            let produce_timeout_flag = is_time_out(my_last_block_timestmp, config.produce_timeout);
+            //console.log("produce time out?", produce_timeout_flag);
+            if (produce_timeout_flag) {
+                restart(node);
+                return;
+            }
+
+        });
+}
+
+setInterval(() => {
+    for (let i = 0; i < config.nodes.length; i++) {
+        check_node(config.nodes[i]);
+    }
+}, 3000);
+
---
+++ tools/mail.js
@@ -0,0 +1,47 @@
+'use strict';
+const nodemailer = require('nodemailer');
+const config = require('../config.json');
+
+// Generate test SMTP service account from ethereal.email
+// Only needed if you don't have a real mail account for testing
+// nodemailer.createTestAccount((err, account) => {
+function sendMail(subject, message) {
+    //替换\n
+    let mailBody = message;
+    let regExp = /\x0a/g; //匹配\n
+    if (message && message != undefined) {
+        mailBody = message.replace(regExp, `</p><p>`);
+    }
+    // create reusable transporter object using the default SMTP transport
+    let mailConfig = config.mail;
+    let transporter = nodemailer.createTransport(mailConfig);
+
+    // setup email data with unicode symbols
+    let mailOptions = {
+        from: '"EOS Monitor" <motorfriends@163.com>', // sender address
+        to: 'qiushaoxi@163.com', // list of receivers
+        subject: subject, // Subject line
+        //text: 'Hello world?', // plain text body
+        html: `<h1>EOS NODE WARNING:</h1>
+        <p>`+ mailBody + `</p>`
+        //html: '<b>Hello world?</b>' // html body
+        //text: message
+        //, // plain text body
+        //html: '<b>Hello world?</b>' // html body
+    };
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+        // Preview only available when sending through an Ethereal account
+        // console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@blurdybloop.com>
+        // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+    });
+}
+
+exports.sendMail = sendMail;
