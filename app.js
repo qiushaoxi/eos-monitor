19bdd4a446a98b92cb98a4157a65d8de45d905fb
qiushaoxi <qiushaoxi@uchain.one>
2018-04-27 09:39:24 +0800

    fix

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var producer = config.producer;
 var my_last_block_timestmp = Date.now();
 
 var restart_flag = false;
+var mail_flag = false;
 
 function is_time_out(time, timeout) {
     let gap = Date.now() - time;
@@ -35,18 +36,30 @@ function safelyParseJSON(json) {
     return parsed // Could be undefined!
 }
 
+function sendmail(title, message) {
+    if (mail_flag) {
+        return;
+    } else {
+        mail_flag = true;
+        mail.sendMail(title, message);
+        setTimeout(() => {
+            restart_flag = false;
+        }, config.mail_time);
+    }
+
+}
 function restart(node) {
     //do not restart when restarting
+    sendMail(node.name, node.url);
     if (restart_flag) {
         return;
     } else {
         console.error("restarting", moment(Date.now()));
-        mail.sendMail(node.name, node.url);
-        shell.exec(node.restart_script);
         restart_flag = true;
+        shell.exec(node.restart_script);
         setTimeout(() => {
             restart_flag = false;
-        }, 60000);
+        }, config.restart_time);
     }
 }
 
@@ -69,15 +82,15 @@ function check_node(node) {
             }
             let result = safelyParseJSON(res.text);
 
-            //console.log('=======================');
-            //console.log(result.head_block_producer);
-            //console.log(result.head_block_num);
+            console.log('=======================');
+            console.log(result.head_block_producer);
+            console.log(result.head_block_num);
 
             let block_timestmp = moment(result.head_block_time + "Z").valueOf();
-            //console.log("time interval from last block:", ((Date.now() - block_timestmp) / 1000).toFixed(2) + " s");
+            console.log("time interval from last block:", ((Date.now() - block_timestmp) / 1000).toFixed(2) + " s");
 
             let timeout_flag = is_time_out(block_timestmp, config.timeout);
-            //console.log("time out?", timeout_flag);
+            console.log("time out?", timeout_flag);
             if (timeout_flag) {
                 restart(node);
                 return;
@@ -87,10 +100,10 @@ function check_node(node) {
             if (result.head_block_producer == producer) {
                 my_last_block_timestmp = block_timestmp
             }
-            //console.log("my_last_block_time:", ((Date.now() - my_last_block_timestmp) / 60000).toFixed(2) + " min")
+            console.log("my_last_block_time:", ((Date.now() - my_last_block_timestmp) / 60000).toFixed(2) + " min")
 
             let produce_timeout_flag = is_time_out(my_last_block_timestmp, config.produce_timeout);
-            //console.log("produce time out?", produce_timeout_flag);
+            console.log("produce time out?", produce_timeout_flag);
             if (produce_timeout_flag) {
                 restart(node);
                 return;
